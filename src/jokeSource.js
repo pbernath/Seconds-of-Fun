bf89f921b83d1edadcfb4242d33e5a52c3810004
wilrob <war.robertsson@hotmail.com>
2022-12-06 17:04:06 +0100

    JokeSource fix

--- src/jokeSource.js
+++ src/jokeSource.js
@@ -1,4 +1,4 @@
-function treatHTTPResponseACB(response){ 
+/*function treatHTTPResponseACB(response){ 
     if(response.status !== 200){
         throw new Error("HTTP error with status: " + response.status);
     }
@@ -8,7 +8,7 @@ function treatHTTPResponseACB(response){
 }
 
 function getJokeFromCat(category){
-    return fetch(" https://v2.jokeapi.dev/joke/Any", {  // object literal
+    return fetch("https://v2.jokeapi.dev/joke/Any", {  // object literal
         "method": "GET"       
      } 
 )
@@ -20,5 +20,27 @@ function responseCB(response){
 
 function getJoke(category){
     return getJokeFromCat(new URLSearchParams(category)).then(treatHTTPResponseACB).then(responseCB);
-}
+}*/
 
+function test(){
+
+console.log("XHR TEST")
+var baseURL = "https://v2.jokeapi.dev";
+var xhr = new XMLHttpRequest();
+xhr.open("GET", baseURL + "/joke/" + "Any?type=single");
+
+xhr.onreadystatechange = function() {
+    if(xhr.readyState == 4 && xhr.status < 300) // readyState 4 means request has finished + we only want to parse the joke if the request was successful (status code lower than 300)
+    {
+        var randomJoke = JSON.parse(xhr.responseText);
+        console.log(randomJoke.joke);
+        //alert(randomJoke.joke);
+    }
+    else if(xhr.readyState == 4)
+    {
+        alert("Error while requesting joke.\n\nStatus code: " + xhr.status + "\nServer response: " + xhr.responseText);
+    }
+};
+
+xhr.send(); 
+}
