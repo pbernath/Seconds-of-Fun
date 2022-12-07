4569e16d5491e41e527796efffbda5a559aee691
Sasha Nsona <nsona@kth.se>
2022-12-07 10:26:00 +0100

    sashasViews to main

--- src/views/ActivityChooser.vue
+++ src/views/ActivityChooser.vue
@@ -5,6 +5,7 @@ import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
+import {joke} from '../jokeSource.js'
 </script>
 
 <template>
@@ -12,34 +13,46 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <DocumentationIcon />
     </template>
-    <template #heading>Choice 0</template>
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Choice 1</template>
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Choice 2</template>
+    <template #heading><button class="button"  v-on:click="joke">Comic strips!</button></template>
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <CommunityIcon />
     </template>
-    <template #heading>Choice 3</template>
+    <template #heading><button class="button"  v-on:click="getJoke">Jokes!</button></template>
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <SupportIcon />
     </template>
-    <template #heading>Choice 4</template>
+    <template #heading><button class="button"  v-on:click="joke">Most liked joke!</button></template>
   </WelcomeItem>
+
 </template>
+<script>export default {
+props:{},
+methods:{getJoke() {joke()}},
+
+
+};
+</script>
+
+<style scoped>
+  .button {
+        background-color:#191a1c;
+        border-color: #5b576b;
+        border-radius: 4px;
+        color:antiquewhite;
+        height:50px; 
+        width: 100px;
+        place-items: center;
+    }
+  .button:hover {
+        background-color:#ffccc5;
+        transition: 0.7s;
+        border-radius: 10px;
+        color:black;
+    }
+</style>
---
+++ src/views/icons/IconJoke.vue
@@ -0,0 +1,6 @@
+<!-- This icon is from <https://github.com/Templarian/MaterialDesign>, distributed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0) license-->
+<template>
+
+<img v-bind:src="('src/views/icons/' + 'LUL.jpg')">
+
+</template>
