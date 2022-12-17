a09ae3a6689364548330f3916d6d0be2c417755d
wilrob <war.robertsson@hotmail.com>
2022-12-17 18:32:37 +0100

    test comics

---
+++ src/components/ActivityChooser.vue
@@ -0,0 +1,66 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+import {joke} from '../configs/jokeSource.js'
+import {randomComic} from '../configs/xkcd.js'
+import { RouterLink, RouterView } from 'vue-router'
+
+
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>
+      <button class="button"  v-on:click="randomComic">
+        Comic strips!
+      </button>
+    </template>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading><button class="button"  v-on:click="getJoke">Jokes!</button></template>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading><button class="button"  v-on:click="joke">Most liked joke!</button></template>
+  </WelcomeItem>
+
+</template>
+<script>export default {
+props:{},
+methods:{getJoke() {randomComic()}},
+
+
+};
+</script>
+
+<style scoped>
+  .button {
+        background-color:#191a1c;
+        border-color: #5b576b;
+        border-radius: 4px;
+        color:antiquewhite;
+        height:50px; 
+        width: 100px;
+        place-items: center;
+    }
+  .button:hover {
+        background-color:#ffccc5;
+        transition: 0.7s;
+        border-radius: 10px;
+        color:black;
+    }
+</style>
