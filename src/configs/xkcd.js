76765c81011e9a354d16eb1545fe04910b84f5b5
AndreasFranke5 <afranke@ug.kth.se>
2022-12-17 17:30:53 +0100

    5xkcd

---
+++ src/configs/xkcd.js
@@ -0,0 +1,103 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+      number: '',
+      max: '',
+      current: {
+        title: '',
+        img: '',
+        alt: ''
+      },
+    loading: true,
+    addedName: '',
+    addedComment: '',
+    comments: {},
+    },
+    created() {
+      this.xkcd();
+    },
+    components: {
+        VueXKCD
+        },
+    computed: {
+        month() {
+          var month = new Array;
+          if (this.current.month === undefined)
+            return '';
+          month[0] = "January";
+          month[1] = "February";
+          month[2] = "March";
+          month[3] = "April";
+          month[4] = "May";
+          month[5] = "June";
+          month[6] = "July";
+          month[7] = "August";
+          month[8] = "September";
+          month[9] = "October";
+          month[10] = "November";
+          month[11] = "December";
+          return month[this.current.month - 1];
+        }
+      },
+
+      watch: {
+        number(value, oldvalue) {
+          if (oldvalue === '') {
+            this.max = value;
+          } else {
+            this.xkcd();
+          }
+          
+        },
+      },
+
+    methods: {
+        addComment() {
+            if (!(this.number in this.comments))
+              Vue.set(app.comments, this.number, new Array);
+            this.comments[this.number].push({
+              author: this.addedName,
+              text: this.addedComment
+            });
+            this.addedName = '';
+            this.addedComment = '';
+          },
+        getRandom(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+          },
+          randomComic() {
+            this.number = this.getRandom(1, this.max);
+          },
+          previousComic() {
+            this.number = this.current.num - 1;
+            if (this.number < 1)
+              this.number = 1;
+          },
+          nextComic() {
+            this.number = this.current.num + 1;
+            if (this.number > this.max)
+              this.number = this.max
+          },
+      async xkcd() {
+        try {
+        this.loading = true;
+        let url = 'https://xkcd.now.sh/?comic=';
+        if (this.number === '') {
+          url += 'latest';
+        } else {
+          url += this.number;
+        }
+        const response = await axios.get(url);
+        this.current = response.data;
+        this.loading = false;
+        this.number = response.data.num;
+          } catch (error) {
+            console.log(error);
+          }
+      }
+    },
+  });
+
+  export {app, created, computed, watch, methods, xkcd, getRandom, randomComic, previousComic, nextComic, addComment, month, number, max, current, loading, addedName, addedComment, comments, Vue, axios}
