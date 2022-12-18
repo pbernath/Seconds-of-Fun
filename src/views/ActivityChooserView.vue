46b065074ba53b0c1c24a4396d8d39c55b0c61f6
AndreasFranke5 <afranke@ug.kth.se>
2022-12-18 17:35:30 +0100

    11

--- src/views/ActivityChooserView.vue
+++ src/views/ActivityChooserView.vue
@@ -8,20 +8,17 @@ import styledButton from "../components/styledButton.vue";
 </script>
 
 <template>
+  <div class="flex-container">
   <div @click="goToHomeACB">
     <li class="pointer">
-		<img v-if="goToHomeACB" src="../assets/sof-logo-500.png" id="homeLogo" width="100" height="100" />
+      <div style="flex: 1 1 100"><img v-if="goToHomeACB" src="../assets/sof-logo-500.png" id="homeLogo" width="100" height="100"></div>
   </li>
 	</div>
-  <div>
-    <styledButton buttonText="Jokes" @click="goToJokesPageACB" />
   </div>
-  <div>
-    <styledButton buttonText="Comics" @click="goToComicACB" />
-  </div>
-  <div>
-    <styledButton buttonText="Login" @click="goToLoginACB" />
-  </div>
+  <div style="flex: 1 1 100"><styledButton buttonText="Jokes" @click="goToJokesPageACB"></styledButton></div>
+  <div style="flex: 1 1 100"><styledButton buttonText="Comics" @click="goToComicACB"></styledButton></div>
+  <div style="flex: 1 1 100"><styledButton buttonText="Login" @click="goToLoginACB"></styledButton></div>
+
 </template>
 
 <script>
@@ -50,6 +47,16 @@ export default {
 </script>
 
 <style>
+.flex-container {
+  display: flex;
+}
+
+.flex-container > div {
+  text-align: center;
+  width: 100px;
+  margin: 0px;
+}
+
 li.pointer {
   cursor: pointer;
 }
